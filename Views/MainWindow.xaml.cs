b19da8336caa65d43d9fb2635751eaada83441e2
NaughtyChas <3030561972@qq.com>
2025-07-22 15:48:23 +0800

    Fix navigation bugs

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
 using Microsoft.UI.Windowing;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Navigation;
 
 namespace GitMC.Views
 {
@@ -9,6 +12,7 @@ namespace GitMC.Views
             this.InitializeComponent();
             SetWindowProperties();
             ContentFrame.Navigate(typeof(HomePage));
+            ContentFrame.Navigated += ContentFrame_Navigated;
         }
 
         private void SetWindowProperties()
@@ -40,18 +44,23 @@ namespace GitMC.Views
         {
             if (args.IsSettingsInvoked)
             {
-                ContentFrame.Navigate(typeof(SettingsPage));
+                if (ContentFrame.CurrentSourcePageType != typeof(SettingsPage))
+                {
+                    ContentFrame.Navigate(typeof(SettingsPage));
+                }
             }
             else
             {
-                // Handle other navigation items if necessary
                 var item = args.InvokedItemContainer;
                 if (item != null)
                 {
                     var tag = item.Tag?.ToString();
                     if (tag == "Home")
                     {
-                        ContentFrame.Navigate(typeof(HomePage));
+                        if (ContentFrame.CurrentSourcePageType != typeof(HomePage))
+                        {
+                            ContentFrame.Navigate(typeof(HomePage));
+                        }
                     }
                     // Potentially handle save-specific navigation here
                 }
@@ -62,7 +71,10 @@ namespace GitMC.Views
         {
             if (args.IsSettingsSelected)
             {
-                ContentFrame.Navigate(typeof(SettingsPage));
+                if (ContentFrame.CurrentSourcePageType != typeof(SettingsPage))
+                {
+                    ContentFrame.Navigate(typeof(SettingsPage));
+                }
             }
             else
             {
@@ -72,10 +84,51 @@ namespace GitMC.Views
                     var tag = selectedItem.Tag?.ToString();
                     if (tag == "Home")
                     {
-                        ContentFrame.Navigate(typeof(HomePage));
+                        if (ContentFrame.CurrentSourcePageType != typeof(HomePage))
+                        {
+                            ContentFrame.Navigate(typeof(HomePage));
+                        }
                     }
                     // Potentially handle save-specific navigation here
                 }
+            }
+        }
+
+        private void TitleBar_BackButtonClick(TitleBar sender, object args)
+        {
+            if (ContentFrame.CanGoBack)
+            {
+                ContentFrame.GoBack();
+            }
+        }
+
+        private void ContentFrame_Navigated(object sender, NavigationEventArgs e)
+        {
+            // NavView.IsBackEnabled = ContentFrame.CanGoBack;
+            // IsBackButtonEnabled 已通过 XAML 绑定到 ContentFrame.CanGoBack
+            UpdateNavigationSelection(e.SourcePageType);
+        }
+
+        private void UpdateNavigationSelection(Type pageType)
+        {
+            if (pageType == typeof(HomePage))
+            {
+                foreach (var item in NavView.MenuItems)
+                {
+                    if (item is NavigationViewItem navItem && navItem.Tag?.ToString() == "Home")
+                    {
+                        NavView.SelectedItem = navItem;
+                        break;
+                    }
+                }
+            }
+            else if (pageType == typeof(SettingsPage))
+            {
+                NavView.SelectedItem = NavView.SettingsItem;
+            }
+            else
+            {
+                NavView.SelectedItem = null;
             }
         }
     }
